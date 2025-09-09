693dfa521b1006386b13bb3ef34c7d57361f3f6d
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-09-09 23:56:40 +0500

    My first commit

--- PowerQ.java
+++ PowerQ.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class PowerQ {
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    // Two numbers are entered by the user, x and n. Write a function to find the value of one number raised to the power of another i.e. xn.
+
 
 System.out.println("Enter the value of x ");
 int x = sc.nextInt();
