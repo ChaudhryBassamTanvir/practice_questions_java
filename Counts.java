629a536254e771f2f47f4d13df53ba104f473b8f
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-09-09 23:51:56 +0500

    My first commit

---
+++ Counts.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Counts {
+    public static void main(String[] args) {
+        
+Scanner sc = new Scanner(System.in);
+
+    ArrayList<Integer> dynamicArray = new ArrayList<>(); // Initial capacity of 10
+
+System.out.println("Enter the amount till where you want to enter numbers ");
+
+int n = sc.nextInt();
+
+int[] numberarray= new int[n];
+
+    for (int i = 0; i <n; i++) {
+
+   numberarray[i]= sc.nextInt();
+        
+    }
+
+
+
+    int positiveInteger= 0;
+    int negativeInteger=0;
+    int countZero=0;
+
+
+    for (int i = 0; i < numberarray.length ; i++) {
+        if (numberarray[i]> 0) {
+            positiveInteger++;
+        } else if (numberarray[i] < 0) {
+           negativeInteger++;
+        } else{
+              countZero++;
+        }
+ 
+
+
+    }
+
+System.out.println("The number of positive Integers is " + positiveInteger);
+System.out.println("The number of negative Integers is " + negativeInteger);
+System.out.println("The number of Zero Integers is " + countZero);
+
+
+
+
+    }
+}
---
+++ InfiniteLoop.java
@@ -0,0 +1,9 @@
+public class InfiniteLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        do{
+            System.out.println("Infinite Loop has begin");
+        i++;
+        }while(i>0);
+    }
+}
