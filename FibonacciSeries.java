a62013c9337ce072427983700ced395889156ca9
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-09-10 22:23:13 +0500

    new file added

---
+++ FibonacciSeries.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter the number till you want Fabonacci Series ");
+int n = sc.nextInt();
+ int num1= 0;
+ int num2= 1;
+
+if (n==0) {
+    System.out.println("0");
+    return;
+}else{
+    for (int i = 0; i <= n; i++) {
+
+        System.out.print( num1   + " "  );
+        int num3=num1+num2;
+        num1=num2;
+        num2=num3;
+
+ 
+}
+}
+
+
+
+    }
+}
